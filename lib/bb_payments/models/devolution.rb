46f52ff135204b9e92820f7b38231e86060accc5
luanbrito7 <luanbritop7@gmail.com>
2023-09-13 17:53:32 -0300

    fix(Devolution): devolution_date is a string, not date

--- lib/bb_payments/models/devolution.rb
+++ lib/bb_payments/models/devolution.rb
@@ -25,7 +25,7 @@ module BancoBrasilPayments
     def self.openapi_types
       {
         reason_code: :Integer,
-        devolution_date: :NumberDate,
+        devolution_date: :String,
         devolution_value: :Float
       }
     end
